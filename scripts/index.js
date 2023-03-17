55b8b2fc7124c5b0e03bd241cdee24ec0fe1055b
KPVakhrushev <KPVakhrushev@gmail.com>
2023-03-17 14:53:04 +0400

    feature: добавить функционал добавления мест

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+const templates         = document.querySelector('#templates').content;
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileTitle      = document.querySelector('.profile__title');
+const profileSubtitle   = document.querySelector('.profile__subtitle');
+const addButton         = document.querySelector('.profile__add-button');
+const popup             = document.querySelector('.popup');
+const popupCloseButton  = document.querySelector('.popup__close');
+const popupForm         = document.querySelector('.popup__form');
+const popupContainer    = document.querySelector('.popup__container');
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -24,40 +34,94 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-let profileEditButton = document.querySelector('.profile__edit-button');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
 
-let popup = document.querySelector('.popup');
-let popupCloseButton   = document.querySelector('.popup__close');
-let popupForm    = document.querySelector('.popup__edit-form');
-let popupInputTitle    = document.querySelector('#popup__text-input_title');
-let popupInputSubtitle = document.querySelector('#popup__text-input_subtitle');
+const forms = {
+  edit:{
+    name: 'profile',
+    action: '/save/profile/',
+    title: 'Редактировать профиль',
+    button: 'Сохранить',
+    fields: {
+      title: {
+        placeholder: 'Введите ваше имя',
+      },
+      subtitle: {
+        placeholder: 'Расскажите немного о себе',
+      }
+    },
+    init: function (){
+      forms.edit.fields.title.element.value    = profileTitle.textContent;
+      forms.edit.fields.subtitle.element.value = profileSubtitle.textContent;
+    },
+    submit: function(event){
+      event.preventDefault();
+      const fields = forms.edit.fields;
+      profileTitle.textContent    = fields.title.element.value;
+      profileSubtitle.textContent = fields.subtitle.element.value;
+      funcPopupClose();
+    }
+  },
+  add:{
+    name: 'addcard',
+    action: '/add/card/',
+    title: 'Новое место',
+    button: 'Создать',
+    fields: {
+      name: {
+        placeholder: 'Название'
+      },
+      src: {
+        placeholder: 'Ссылка на картинку'
+      }
+    },
+    init: () => {},
+    submit: event => {
+      event.preventDefault();
+      funcAddCard(forms.add.fields.name.element.value, forms.add.fields.src.element.value);
+      funcPopupClose();
+    }
+  }
+}
 
-
-function popupClose(){
+const funcPopupClose     = function (){
   popup.classList.remove('popup_opened');
 }
-profileEditButton.addEventListener('click', function(){
-  popup.classList.add('popup_opened');
-  popupInputTitle.value = profileTitle.textContent;
-  popupInputSubtitle.value = profileSubtitle.textContent;
-});
-popupCloseButton.addEventListener('click', popupClose);
-popupForm.addEventListener('submit', function(evt){
-  evt.preventDefault();
-  profileTitle.textContent = popupInputTitle.value;
-  profileSubtitle.textContent = popupInputSubtitle.value;
-  popupClose();
-});
-
-function addCard(name, link){
-  const template   = document.querySelector('#card').content;
-  const newElement = template.querySelector('.element').cloneNode(true);
+const funcCloneTemplate  = function (className, attrubutes=null){
+  const newElement = templates.querySelector(className).cloneNode(true);
+  if(attrubutes){
+    for(const [name, value] of Object.entries(attrubutes)){
+      newElement[name] = value;
+    }
+  }
+  return newElement;
+}
+const funcAddCard        = function (name, link){
+  newElement = funcCloneTemplate('.element');
   newElement.querySelector('.element__image').src = link;
   newElement.querySelector('.element__title').textContent = name;
   document.querySelector('.elements').append(newElement);
   return newElement;
 }
+const functOpenForm      = function (formData){
+  popup.classList.add('popup_opened');
+  popupForm.name      = formData.name;
+  popupForm.action    = formData.action;
+  popupForm.innerHTML = '';
 
-initialCards.forEach(card => addCard(card.name, card.link));
+  popupForm.append(funcCloneTemplate('.popup__title', {textContent: formData.title}));
+  let element;
+  for(const [name, field] of Object.entries(formData.fields)){
+    element = funcCloneTemplate('.popup__text-input', {name: name, placeholder: field.placeholder});
+    popupForm.append(element);
+    formData.fields[name].element = element;
+  }
+  const saveButton   = funcCloneTemplate('.popup__save-button', {textContent: formData.button, title: formData.button});
+  saveButton.addEventListener('click', (event)=>formData.submit(event));
+  popupForm.append(saveButton);
+  formData.init();
+}
+
+profileEditButton.addEventListener('click', ()=>functOpenForm(forms.edit));
+addButton.addEventListener('click', ()=>functOpenForm(forms.add));
+popupCloseButton.addEventListener('click', funcPopupClose);
+initialCards.forEach(card => funcAddCard(card.name, card.link));
